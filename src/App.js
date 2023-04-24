ac1c3d9765dc732540568f1172a803845b65644e
CharlesZim <charles.zimmerlin@gmail.com>
2023-04-24 18:20:17 +0200

    changed circle side

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ const Main = () => {
         <Services />
         <Contact />
         <CircleOpacity color="rgba(200,100,100,0.1)" />
-        <CircleOpacity color="rgba(200,100,100,0.1)" />
         <CircleOpacity left section={1} color="rgba(255,128,255,0.1)" />
         <CircleOpacity section={2} color="rgba(255,255,0,0.1)" />
         <CircleOpacity left section={3} color="rgba(255,255,255,0.1)" />
